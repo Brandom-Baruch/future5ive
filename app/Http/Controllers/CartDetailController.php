cf45c9fe471c429a1196636c6fbd936a96204072
Brandom Gonzalez Cervantes <baruchoo94@gmail.com>
2020-04-22 15:43:48 -0500

    Add guzzle

--- app/Http/Controllers/CartDetailController.php
+++ app/Http/Controllers/CartDetailController.php
@@ -3,21 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\CartDetail;
+
 
 class CartDetailController extends Controller
 {
     public function store (Request $request)
-    {
-    	$cartDetail = new CartDetail();
-        $cartDetail->cart_id = auth()->user()->cart->id;
+    {               
+        
+        $cartDetail = new CartDetail();        
+        $cartDetail->cart_id = auth()->user()->cart->id;        
         $cartDetail->product_id = $request->product_id;
         $cartDetail->quantity = $request->quantity;
         $cartDetail->save();
         
         $notification = 'Se ha agregado el producto a tu carrito de compras correctamente.';
         return back()->with(compact('notification'));
+        
     }
 
     public function destroy (Request $request)
